7aab7f3fac25657a8607c149abf62291b2811be7
darren9897 <darrenkoomson17@gmail.com>
2020-07-04 17:47:31 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// function superbowlWin(arr){
+//   arr.find(team=> if (team.result === "W") => team.year)
+// }
+
+function superbowlWin(arr) {
+  let yearWon
+  arr.find(win => {
+    if (win.result === 'W') {
+      yearWon = win.year
+    }
+  })
+  return yearWon
+}
